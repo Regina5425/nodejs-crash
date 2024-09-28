0dfda5c5bc580f019afcea4c3d11d6cc060359f8
Regina <reina5425@gmail.com>
2024-09-28 19:45:24 +0300

    simple api

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
 import http from 'http';
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
 
 const PORT = process.env.PORT;
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
 	// res.setHeader('Content-Type', 'text/html')
 	try {
 		if(req.method === 'GET') {
+			let filePath;
 			if(req.url === '/') {
-				res.writeHead(200, {'Content-Type': 'text/html'})
-				res.end('<h1>Homepage</h1>');
+				filePath = path.join(__dirname, 'public', 'index.html')
 			} else if(req.url === '/about') {
-				res.writeHead(200, {'Content-Type': 'text/html'})
-				res.end('<h1>About</h1>');
+				filePath = path.join(__dirname, 'public', 'about.html')
 			} else {
-				res.writeHead(404, {'Content-Type': 'text/html'})
-				res.end('<h1>Not Found</h1>');
+				throw new Error('Not Found')
 			}
+
+			const data = await fs.readFile(filePath);
+			res.setHeader('Content-Type', 'text/html');
+			res.write(data)
+			res.end();
 		} else {
 			throw new Error('Method not allowed');
 		}
