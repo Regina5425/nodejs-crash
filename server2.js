caef289e979d32000627ecbadeac6a6e1133d0fe
Regina <reina5425@gmail.com>
2024-09-28 20:14:48 +0300

    post method create

--- server2.js
+++ server2.js
@@ -41,6 +41,22 @@ const getUserByIdHendler = (req, res) => {
 	res.end();
 }
 
+// Route handler for POST /api/users
+const createUserHandler = (req, res) => {
+	let body = '';
+	// listen for data
+	req.on('data', (chunk) => {
+		body += chunk.toString();
+	});
+	req.on('end', () => {
+		const newUser = JSON.parse(body);
+		users.push(newUser);
+		res.statusCode = 201;
+		res.write(JSON.stringify(newUser));
+		res.end();
+	})
+}
+
 // Not foound handler
 const notFoundHandler = (req, res) => {
 	res.statusCode = 404;
@@ -55,6 +71,8 @@ const server = createServer((req, res) => {
 				getUsersHandler(req, res);
 			} else if(req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET') {
 				getUserByIdHendler(req, res);
+			} else if(req.url === '/api/users' && req.method === 'POST') {
+				createUserHandler(req, res);
 			} else {
 				notFoundHandler(req, res);
 			}
